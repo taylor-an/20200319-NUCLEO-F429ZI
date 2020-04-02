e1316f43a4a83213fd8705653f458e57a8df96ae
taylor-an <taylor@wiznet.io>
2020-04-02 11:19:57 +0900

    Modified Device Configuration Tool LWIP  Middleware -> LWIP -> Key Options -> Infrastructure - Heap and Memory Pool Options -> MEM_SIZE (Heap Memory Size) -> 3200 Byte(s)

--- LWIP/Target/lwipopts.h
+++ LWIP/Target/lwipopts.h
@@ -53,6 +53,8 @@
 #define LWIP_DNS 1
 /*----- Value in opt.h for MEM_ALIGNMENT: 1 -----*/
 #define MEM_ALIGNMENT 4
+/*----- Default Value for MEM_SIZE: 1600 ---*/
+#define MEM_SIZE 3200
 /*----- Value in opt.h for LWIP_ETHERNET: LWIP_ARP || PPPOE_SUPPORT -*/
 #define LWIP_ETHERNET 1
 /*----- Value in opt.h for LWIP_DNS_SECURE: (LWIP_DNS_SECURE_RAND_XID | LWIP_DNS_SECURE_NO_MULTIPLE_OUTSTANDING | LWIP_DNS_SECURE_RAND_SRC_PORT) -*/
